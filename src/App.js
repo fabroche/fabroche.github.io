3c1167c85509c02c129fc98204859fe909df8446
Kraxus998 <kraxusmmo@gmail.com>
2023-04-12 15:22:43 -0700

    Deploy settings

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// estilos
+import './App.css'
+
+// assets
+import freeCodeCampLogo from './images/freecodecamp-logo.png'
+
+// components
+import LogoFreeCodeCamp from './components/LogoFreeCodeCamp'
+import TodoList from './components/TodoList'
+
+// hooks
+
+
+function App() {
+
+
+  return (
+    <div className="aplicacion-tareas">
+      <LogoFreeCodeCamp src={freeCodeCampLogo}/>
+      <TodoList titulo="My To-Do's"/>
+
+    </div>
+  )
+}
+
+export default App
